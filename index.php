38a6254752f411f888a3eea487db141981221327
Jamallc <weberrocha15@gmail.com>
2021-02-22 10:51:36 -0300

    Aula de frontend

--- index.php
+++ index.php
@@ -97,5 +97,61 @@ and open the template in the editor.
             </table>
         </div>
 
+        <h1>Vídeos</h1>
+        <iframe id="video" width="360" height="215" src="https://www.youtube.com/embed/oZ7VgXM63gc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <h1>Mapas - geolocalização</h1>
+        <iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.1455185741065!2d-47.892343585145035!3d-15.796279189049995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3ae665464fb7%3A0x29d89d9adf83e13f!2sSenac%20Jess%C3%A9%20Freire%20(DF)!5e0!3m2!1spt-BR!2sbr!4v1613996258008!5m2!1spt-BR!2sbr" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <h1>Áudio</h1>
+        <audio controls>
+            <source src="" type="audio/mp3">
+        </audio>
+        <h1>Formulários</h1>
+        <form action="" method="post">
+            <fieldset>
+                <legend>Formulário de cadastro</legend>
+                <label>Nome:</label>
+                <input type="text" placeholder="digite seu nome"><br>
+                <label>Sobrenome:</label>
+                <input type="text" placeholder="digite seu sobrenome"><br>
+                <label>Idade:</label>
+                <input type="number"><br>
+                <label>Telefone</label>
+                <input type="tel"><br>
+                <label>Data de Nascimento</label>
+                <input type="date"><br>
+                <label>Genero:</label><br>
+                <input type="radio" name="genero" value="masculino">
+                <label>Masculino</label>
+                <input type="radio" name="genero" value="feminino">
+                <label>Feminino</label>
+                <input type="radio" name="genero" value="outros">
+                <label>Outros</label><br>
+                <label>Transportes:</label><br>
+                <input type="checkbox" value="bike">
+                <label>Bicicleta</label>
+                <input type="checkbox" value="moto">
+                <label>Moto</label>
+                <input type="checkbox" value="carro">
+                <label>Carro</label>
+                <input type="checkbox" value="publico">
+                <label>Público</label><br>
+                <label>Cidade Satélite</label><br>
+                <select size="5">
+                    <option>Selecione...</option>
+                    <option value="aguasclaras">Águas Claras</option>
+                    <option value="brazlandia">Brazlândia</option>
+                    <option value="ceilandia">CeiLândia</option>
+                    <option value="cruzeiro">Cruzeiro</option>
+                    <option value="estrutural">Estrutural</option>
+                    <option value="gama">Gama</option>
+                    <option value="guara">Guará</option>
+                </select><br>
+                <label>Mensagem:</label><br>
+                <textarea style="width:400px; height:200px;"></textarea><br>
+                <input type="submit" value="Cadastrar">
+                <input type="button" value="Limpar">
+                <button type="button">Cancelar</button>
+            </fieldset>
+        </form>
     </body>
 </html>
