e1413160754ab40810f1e71e689546c89a163b05
Jamallc <weberrocha15@gmail.com>
2021-02-22 08:21:23 -0300

    Aula de frontend

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Minha primeira página</title>
+        <link rel="shortcut icon" href="icon/camera.png"> 
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <h1>Meu primeiro título</h1>
+        <div id="esquerda">
+            <p id="primeiroparagrafo"><strong> <b>Lorem Ipsum</b> is simply dummy text of the 
+                    <a href="https://www.google.com" target="blanck"> 
+                        printing </a> and typesetting industry.</strong> 
+                <mark>Lorem Ipsum has been the industry's standard dummy text ever </mark>
+                since the 1500s, when an unknown printer took a galley of type 
+                and scrambled it to make a type specimen book.<del> It has survived 
+                    not only five <ins>centuries, but also the leap into electronic</ins> 
+                    typesetting, remaining essentially unchanged.</del> It was <i>popularised 
+                    in the 1960s</i> with the release of Letraset sheets containing Lorem 
+                Ipsum passages, and more recently with desktop publishing software 
+                like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div id="centro">
+            <p>
+                <img src="img/noteGamer.jpg" title="Notebook Acer Gamer" alt="notebook gamer">
+            </p>
+        </div>
+        <div id="direita">
+            <p class="segundoparagrafo">
+                Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </p>
+        </div>
+        <h1>Lista de Compras</h1>
+        <div>
+            <ul>
+                <li class="uva">Uva</li>
+                <li>Banana</li>
+                <li>Abacate</li>
+                <li>Abacaxi</li>
+                <li>Maçã</li>
+                <li>Morango</li>
+                <li>Batata</li>
+                <li>Tomate</li>
+            </ul>
+        </div>
+        <h1>Tabela de notas</h1>
+        <div class="tabela">
+            <table>
+                <tr>
+                    <th class="aluno">Aluno</th>
+                    <th>Nota1</th>
+                    <th>Nota2</th>
+                    <th>Nota3</th>
+                    <th>Nota4</th>
+                    <th>Média</th>
+                </tr>
+                <tr>
+                    <td class="aluno">Ralph</td>
+                    <td>7.0</td>
+                    <td>5.5</td>
+                    <td>6.0</td>
+                    <td>4.5</td>
+                    <td>8.0</td>
+                </tr>
+                <tr>
+                    <td class="aluno">João</td>
+                    <td>7.0</td>
+                    <td>5.5</td>
+                    <td>6.0</td>
+                    <td>4.5</td>
+                    <td>8.0</td>
+                </tr>
+                <tr>
+                    <td class="aluno">Bruna</td>
+                    <td>7.0</td>
+                    <td>5.5</td>
+                    <td>6.0</td>
+                    <td>4.5</td>
+                    <td>8.0</td>
+                </tr>
+                <tr>
+                    <td class="aluno">Lucas</td>
+                    <td>7.0</td>
+                    <td>5.5</td>
+                    <td>6.0</td>
+                    <td>4.5</td>
+                    <td>8.0</td>
+                </tr>
+            </table>
+        </div>
+
+    </body>
+</html>
